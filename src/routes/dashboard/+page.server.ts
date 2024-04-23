1fd0fb2fec98b5073e37962493e70e5af46fba80
itsrusty <rusty3031@gmail.com>
2024-04-23 13:55:38 -0700

    feat: nuevo dise;o

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,36 +1,38 @@
-import type { PageServerLoad } from "./$types";
-import { redirect } from "@sveltejs/kit";
-import jwt from "jsonwebtoken";
-import { userIdStore } from "$lib/store/store";
+// import type { PageServerLoad } from "./$types";
+// import { redirect } from "@sveltejs/kit";
+// import { userIdStore } from "$lib/store/store";
+// import jwt from "jsonwebtoken";
 
-export const load: PageServerLoad = async ({ cookies }) => {
-  const token = cookies.get("jwt");
-  const decodeToken = jwt.decode(token || "");
-  // @ts-expect-error
-  const userId = decodeToken.userId;
+// export const load: PageServerLoad = async ({ cookies }) => {
+//   const token = cookies.get("jwt");
+//   const decodeToken = jwt.decode(token || "");
+//   // @ts-expect-error
+//   const userId = decodeToken.userId;
 
-  console.log(decodeToken);
-  console.log(userId);
+//   console.log(decodeToken);
+//   console.log(userId);
   
-  try {
-    if (cookies === undefined) throw new Error("no existe el token");
-    const validateSessionUser = await fetch(
-      "http://localhost:5000/api/v1/info",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          Cookie: `jwt=${token};`,
-        },
-        credentials: "include",
-      }
-    );
+//   try {
+//     if (cookies === undefined) throw new Error("no existe el token");
+//     const validateSessionUser = await fetch(
+//       "http://localhost:5000/api/v1/info",
+//       {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//           Cookie: `jwt=${token};`,
+//         },
+//         credentials: "include",
+//       }
+//     );
 
-    // console.log(validateSessionUser);
-    if (!validateSessionUser.ok) throw new Error("error al intentar acceder");
+//     // console.log(validateSessionUser);
+//     if (!validateSessionUser.ok) throw new Error("error al intentar acceder");
 
-    userIdStore.set(userId)
+//     userIdStore.set(userId)
 
-  } catch (error) {
-    return redirect(303, "/auth/login");
-  }
-};
+//   } catch (error) {
+//     console.log(error);
+    
+//     return redirect(303, "/auth/login");
+//   }
+// };

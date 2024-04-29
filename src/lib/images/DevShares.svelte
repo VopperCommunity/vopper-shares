c015695dba541e054406b55507f46fc15cad4de8
leochmoc <leochmoc@gmail.com>
2024-04-28 23:53:43 -0500

    add automatic run dev

--- src/lib/images/DevShares.svelte
+++ src/lib/images/DevShares.svelte
@@ -1,6 +1,6 @@
 <article class="flex items-center h-24 gap-4 lg:scale-100 scale-130">
-  <section class="rounded-full flex justify-end p-3 w-20 h-20 bg-white relative">
-    <div class="rounded-full w-8 h-8 bg-[#1D2128FF]"></div>
+  <section class="rounded-full flex justify-end p-3 size-20 bg-white relative">
+    <div class="rounded-full size-8 bg-[#1D2128FF]"></div>
     <span class="bg-white w-[.65rem] h-[.65rem] absolute top-0 right-0 rounded-full"></span>
   </section>
   <strong class="text-4xl">
